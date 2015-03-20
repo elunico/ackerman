3672633af4a1dd7d06eb997bf5c918c8344db8a1
Thomas <tompov227@gmail.com>
2015-03-20 19:04:34 -0400

    more acks (working)

---
+++ ack.swift
@@ -0,0 +1,20 @@
+#!/usr/bin/xcrun swift
+func ack(m: Int, n: Int) -> Int {
+    if ( m == 0) {return n + 1}
+    else if ( n == 0 ) {return ack(m - 1, 1)}
+    else {return ack(m - 1, ack(m, n - 1))}
+}
+
+func main() {
+    for (var i = 0; i < 5; i++) {
+        for (var j = 0; j < 5; j++) {
+            var ans: Int = ack(i, j)
+            println("The ack of \(i) and \(j) is \(ans)")
+            if i == 4 && j == 1 {
+                break
+            }
+        }
+    }
+}
+
+main()
