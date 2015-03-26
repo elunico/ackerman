5f98899e28851c4f72e4e1b66e5957b3c1cdfd7b
Thomas <tompov227@gmail.com>
2015-03-26 15:39:51 -0400

    Currently Broken

--- ack.cpp
+++ ack.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 
-using namespace std;
 int ack(int m, int n) {
-    if ( m == 0) return n + 1;
-    else if ( n == 0 ) return ack(m - 1, 1);
-    else return ack(m - 1, ack(m, n - 1));
+    if ( m == 0) {return n + 1;}
+    else if ( n == 0 ) {return ack(m - 1, 1);}
+    else {return ack(m - 1, ack(m, n - 1));}
 }
 
 int main (int argc, char const *argv[])
@@ -13,7 +12,7 @@ int main (int argc, char const *argv[])
         for (int j = 0; j < 5; j++) {
             if (i == 4 && j == 2) break; // C++ stack limit
             int ans = ack(i, j);
-            cout << "The ack of " << i << " and " << j << " is " << ans << endl;
+            std::cout << "The ack of " << i << " and " << j << " is " << ans << std::endl;
         }
     }
     return 0;
