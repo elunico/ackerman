2496960ec22dc5865f18b58dec9852967be4e19a
Thomas <tompov227@gmail.com>
2015-04-07 12:51:21 -0400

    fixed php

--- ack.c
+++ ack.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int ack(int m, int n) {
-    if ( m == 0) return n + 1;
+    if ( m == 0 ) return n + 1;
     else if ( n == 0 ) return ack(m - 1, 1);
     else return ack(m - 1, ack(m, n - 1));
 }
--- ack.php
+++ ack.php
@@ -1,38 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Ackermann</title>
 
-    <style type="text/css" media="screen">
-        body {
-            font-family: "Helvetica Neue", Arial, Helvetica, sans-serif;
-            font-size: 16pt;
-            color: #222;
-            margin: 2%;
-        }
-    </style>
+<?php
 
-</head>
-<body>
-    <?php
+function ack($m, $n) {
+  if ($m == 0) {
+    return $n + 1;
+  } else if ($n == 0) {
+      return ack($m - 1, 1);
+  } else {
+      return ack($m - 1, ack($m, $n - 1));
+  }
+}
 
-    function ack($m, $n) {
-      if ($m == 0) {
-        return $n + 1;
-      } else if ($n == 0) {
-          return ack($m - 1, 1);
-      } else {
-          return ack($m - 1, ack($m, $n - 1));
-      }
+for ($i = 0; $i < 5; $i++) {
+    for ($j = 0; $j < 5; $j++) {
+        echo "The ack of " . $i . " and " . $j . " is " . ack($i, $j);
+        echo "\n";
+        if ($i == 4 && $j == 0) break;
     }
+}
 
-    for ($i = 0; $i < 5; $i++) {
-        for ($j = 0; $j < 5; $j++) {
-            echo "The ack of " . i . " and " . j . " is " . ack($i, $j);
-        }
-    }
-
-     ?>
-</body>
-</html>
+ ?>
