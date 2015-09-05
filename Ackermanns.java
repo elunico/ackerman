3f3890cf97910b5d4a44bf6d1d59e357d317d653
Thomas <tompov227@gmail.com>
2015-09-05 00:18:32 -0400

    renamed java

--- Ackermanns.java
+++ Ackermanns.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Ackermanns {
     public static int Ackermann(int x, int y) {
         if (x == 0) return y + 1;
         else if (y == 0) return Ackermann(x - 1, 1);
