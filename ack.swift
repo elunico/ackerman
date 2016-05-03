efe3eb02b0b0aa547d6cf4b9dc55a45988b7ccc7
Tom <tompov227@gmail.com>
2016-05-03 14:05:50 -0400

    Removed C style for loops; updated method call syntax for 2.1

--- ack.swift
+++ ack.swift
@@ -1,15 +1,15 @@
 #!/usr/bin/xcrun swift
 func ack(m: Int, n: Int) -> Int {
     if ( m == 0) {return n + 1}
-    else if ( n == 0 ) {return ack(m - 1, 1)}
-    else {return ack(m - 1, ack(m, n - 1))}
+    else if ( n == 0 ) {return ack(m - 1, n: 1)}
+    else {return ack(m - 1, n: ack(m, n: n - 1))}
 }
 
 func main() {
-    for (var i = 0; i < 5; i++) {
-        for (var j = 0; j < 5; j++) {
-            var ans: Int = ack(i, j)
-            println("The ack of \(i) and \(j) is \(ans)")
+    for i in 0..<5 {
+        for j in 0..<5 {
+            let ans: Int = ack(i, n: j)
+            print("The ack of \(i) and \(j) is \(ans)")
             if i == 4 && j == 1 {
                 break // Stack limit for Swift
             }
